9f86a0dbd74998101fee1ee05c642f6048364492
AlejandroFDLV <alejandro.ucla@outlook.com>
2024-07-18 23:32:34 -0400

    (20240718-2330-A)

--- js/Cl_Empresa.js
+++ js/Cl_Empresa.js
@@ -7,7 +7,7 @@ export default class Cl_Empresa{
     }
     procesarTrabajador(t){
         this.acumHE+=t.hE*25  
-        if(t.hR>0 || t.hE>0){this.sueldo=((t.hR*10)+(t.hE*25))}
+        this.sueldo=((t.hR*10)+(t.hE*25))
         if(this.sueldo<this.sM){this.sM=this.sueldo; this.nM=t.nombre}
     }
     montoTotalExtra(){return this.acumHE}
